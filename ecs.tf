e49c6b7020e60bda67f28643f2624928c736107b
Amanda Mattos <amandamattos@gmail.com>
2018-09-27 23:50:04 -0300

    creating task definition

--- ecs.tf
+++ ecs.tf
@@ -17,108 +17,39 @@ resource "aws_ecr_repository" "tw-teste-ecr" {
   name = "tw-teste-ecr"
 }
 
-data "template_file" "tw_teste_app_task" {
-  template = "${file("tw-teste-app-task.json")}"
 
-  vars {
-    image               = "${aws_ecr_repository.tw-teste-ecr.repository_url}"
-    container_name      = "${var.container_name}"
-    container_port      = "3000"
-    log_group           = "${aws_cloudwatch_log_group.tw-web-app.name}"
-    desired_task_cpu    = "2048"
-    desired_task_memory = "4096"
-  }
-}
 
 # Cluster Execution Role
 resource "aws_iam_role" "ecs_execution_role" {
   name               = "ecs_task_execution_role"
   assume_role_policy = "${file("policies/ecs-task-execution-role.json")}"
 }
+*/
 
 # ECS Task definition
+/* data "template_file" "tw_teste_app_task" {
+  template = "${file("tw-teste-task-snapshot.json")}"
+
+  vars {
+    image               = "amandamattos/tw_teste_app:snapshot"
+    container_name      = "tw_teste_app"
+    container_port      = "3000"
+    desired_task_cpu    = "1024"
+    desired_task_memory = "2048"
+  }
+} */
+
 resource "aws_ecs_task_definition" "tw-teste-app-task" {
-  family                   = "tw_teste_app"
-  container_definitions    = "${data.template_file.tw_teste_app_task.rendered}"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
+  family                   = "tw_teste_app1"
+  requires_compatibilities = ["EC2"]
   cpu                      = "2048"
   memory                   = "4096"
-
-  execution_role_arn = "${aws_iam_role.ecs_execution_role.arn}"
-  task_role_arn      = "${aws_iam_role.ecs_execution_role.arn}"
-}
-*/
-
-/*
-# ECS Service Policy
-data "aws_iam_policy_document" "ecs_service_policy" {
-  statement {
-    effect    = "Allow"
-    resources = ["*"]
-
-    actions = [
-      "elasticloadbalancing:Describe*",
-      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-      "ec2:Describe*",
-      "ec2:AuthorizeSecurityGroupIngress",
-    ]
-  }
+  container_definitions = "${file("task-definitions/tw-teste-task-snapshot.json")}"
+  #execution_role_arn = "${aws_iam_role.ecs_execution_role.arn}"
+  #task_role_arn      = "${aws_iam_role.ecs_execution_role.arn}"
 }
 
-data "aws_iam_policy_document" "ecs_service_role" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "ecs_role" {
-  name               = "ecs_role"
-  assume_role_policy = "${data.aws_iam_policy_document.ecs_service_role.json}"
-}
-
-resource "aws_iam_role_policy" "ecs_service_role_policy" {
-  name   = "ecs_service_role_policy"
-  policy = "${data.aws_iam_policy_document.ecs_service_policy.json}"
-  role   = "${aws_iam_role.ecs_role.id}"
-}
-
-# ECS Service definition
-resource "aws_ecs_service" "teste-tw-service" {
-  name            = "teste-tw-service"
-  task_definition = "tw_teste_task_snapshot" #"${aws_ecs_task_definition.tw-teste-app-task.arn}"
-  cluster         = "teste-cluster" #"${aws_ecs_cluster.tw-cluster.id}"
-  launch_type     = "EC2"
-  desired_count   = "2"
-
-  depends_on = ["aws_iam_role_policy.ecs_service_role_policy"]
-
-  network_configuration {
-    security_groups  = ["${aws_security_group.tw_ecs_service_sg.id}"]
-    subnets          = ["subnet-07ef91e6e6b8d0d41","subnet-0a8fdbfa4ff3b6cc7"] #["${aws_subnet.twPublicSubnet1.id}", "${aws_subnet.twPublicSubnet2.id}"]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = "${aws_alb_target_group.twTesteAppTG1.arn}"
-    container_name   = "${var.container_name}"
-    container_port   = "${var.container_port}"
-  }
-
-  depends_on = ["aws_alb_target_group.twTesteAppTG1"]
-}
-
-*/
-
-
-
+# ECS Cluster Service Definition
 resource "aws_iam_role" "ecs-service-role" {
     name                = "ecs-service-role"
     path                = "/"
